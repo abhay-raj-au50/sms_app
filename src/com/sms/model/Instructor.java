44f9199d1480c50de378fe5d036ccf442da3d3a8
abhay-au-50 <abhaycri9@gmail.com>
2024-10-23 12:28:57 +0530

    sms app initial commit

---
+++ src/com/sms/model/Instructor.java
@@ -0,0 +1,32 @@
+package com.sms.model;
+
+public class Instructor {
+	private int id;
+	private String name;
+	private String salary;
+	private String contact;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+}
